24a4de576136b25b3c96d3153282d06f5f886e58
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2021-02-10 15:40:37 +0200

    4 shapes

--- shapes.js
+++ shapes.js
@@ -66,12 +66,36 @@ class circle extends polygon {
         ctx.arc(this.x1, this.y1, this.radius, 0, 2 * Math.PI);
         ctx.stroke();
     }
-    
+
     area() {
         return Math.PI * this.radius * this.radius;
     }
 }
 
+class triangle extends polygon {
+    constructor(x1, y1, x2, y2, x3, y3) {
+        super(x1, y1, x2, y2);
+        this.x3 = x3;
+        this.y3 = y3;
+    }
 
-let c = new circle(500,500,25);
-c.draw();
+    draw() {
+        var c = document.getElementById("cnv");
+        var ctx = c.getContext("2d");
+        
+        ctx.fillStyle = "black";
+        ctx.beginPath();
+        ctx.moveTo(this.x1, this.y1);
+        ctx.lineTo(this.x2, this.y2);
+        ctx.lineTo(this.x3, this.y3);
+        ctx.lineTo(this.x1, this.y1);
+        ctx.stroke();
+    }
+    area() {
+        return 1/2[this.x1(this.y2 - this.y3) + this.x2(this.y3 - this.y1) + this.x3(this.y1 - this.y2)]
+    }
+}
+
+let t = new triangle(0,0,0,100,100,100);
+t.draw();
+console.log(t.toString());
